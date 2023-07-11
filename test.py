5d46420f7c1b017f1f1631cce7a0596f917c1906
KTom101 <kyra.thompson@nyu.edu>
2023-07-11 15:00:06 -0400

    modify test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 def generateDocsURL(file_path):
+  # Docs
   url = "https://www.codecademy.com/resources/docs/"
   print (url)
 
 def generatePrURL(number):
+  # Pull
   url = "https://github.com/Codecademy/docs/pull/"
   print (url + str(number))
