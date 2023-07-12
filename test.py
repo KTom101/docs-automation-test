a88cc93f94f0845d76a304c956ff21571a9189c8
Christine Yang <yang.christine.95@gmail.com>
2023-07-12 11:20:10 -0400

    Add todos as comments

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 def generateDocsURL(file_path):
   url = "https://www.codecademy.com/resources/docs/"
   path_parts = file_path.split("/")
+  # Add cases for files that are in the documentation folder
   for part in path_parts:
     if part == "concepts" or part == "content" or part == "terms" or part.endswith(".md"):
       path_parts.remove(part)
+  # end cases
   path_parts = "/".join(path_parts)
   print (url + path_parts)
 
