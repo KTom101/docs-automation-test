93060a727ee98228e662afca80605d6f4fe8d4db
Christine Yang <yang.christine.95@gmail.com>
2023-07-31 18:33:08 -0400

    Remove comments in source file

--- source.py
+++ source.py
@@ -1,16 +1,11 @@
 from test import *
 
-data = [] # Array of objects
+data = []
 
 for item in data:
-  # Access the value under the "number" key and print it
   number_value = item["node"]["number"]
   print(generatePrURL(number_value))
-
-  # Access the "edges" array
   edges = item["node"]["files"]["edges"]
-  # Loop through each item in the "edges" array
   for edge in edges:
-    # Access the "path" key and print its value
     path_value = edge["node"]["path"]
     print(generateDocsURL(path_value))
